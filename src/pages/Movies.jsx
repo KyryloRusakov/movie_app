b4e72e4b31c62f37eb6df6feb22338bc70d123b2
Kyrylo Rusakov <kyrylo.rusakov@nixsolutions.com>
2023-05-25 18:50:39 +0300

    fetch list of movies and display title and posters

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Movies = () => {
-  return (
-    <div>Movies</div>
-  )
-}
+  const [movies, setMovies] = useState([]);
 
-export default Movies
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const apiKey = 'fab30af0c86949df3573cee27a305bb0';
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
+        );
+        const data = await response.json();
+        setMovies(data.results);
+        console.log(data.results);
+      } catch (error) {
+        console.error("Ошибка при получении списка фильмов:", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+
+  return (
+    <div>
+      Movies
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <img src={imageBaseUrl + movie.poster_path} alt={movie.title} />
+            {movie.title}
+            {movie.release_date}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Movies;
