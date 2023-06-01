8f4d528afe3b76f4e6ab99d8d563632bafe68f32
Kyrylo Rusakov <kyrylo.rusakov@nixsolutions.com>
2023-06-01 19:47:17 +0300

    create pagination for movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -7,20 +7,24 @@ const Movies = () => {
   const [languages, setLanguages] = useState([]);
   const [selectedLanguage, setSelectedLanguage] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const apiKey = "fab30af0c86949df3573cee27a305bb0";
-        let apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+        let apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${currentPage}`;
 
         if (searchQuery) {
-          apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`;
+          apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}&page=${currentPage}`;
         }
 
         const response = await fetch(apiUrl);
         const data = await response.json();
         setMovies(data.results);
+        //setTotalPages(data.total_pages) max 500 pages, but total_pages = 38545;
+        setTotalPages(500);
         console.log(data);
       } catch (error) {
         console.error("Ошибка при получении списка фильмов:", error);
@@ -57,7 +61,7 @@ const Movies = () => {
     fetchMovies();
     fetchGenres();
     fetchLanguages();
-  }, [searchQuery]);
+  }, [searchQuery, currentPage]);
 
   const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
 
@@ -69,9 +73,77 @@ const Movies = () => {
     setSelectedLanguage(event.target.value);
   };
 
-    const handleSearchInputChange = (event) => {
-      setSearchQuery(event.target.value);
-    };
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+ const handlePageChange = (page) => {
+   setCurrentPage(page);
+ };
+
+ const renderPageNumbers = () => {
+   const pageNumbers = [];
+
+   // Добавляем первую страницу
+   pageNumbers.push(
+     <button
+       key={1}
+       onClick={() => handlePageChange(1)}
+       disabled={currentPage === 1}
+     >
+       1
+     </button>
+   );
+
+   // Добавляем страницы между первой и последней
+   let startPage = currentPage - 2;
+   let endPage = currentPage + 2;
+
+   if (startPage <= 1) {
+     startPage = 2;
+     endPage = Math.min(startPage + 4, totalPages - 1);
+   }
+
+   if (endPage >= totalPages) {
+     endPage = totalPages - 1;
+     startPage = Math.max(endPage - 4, 2);
+   }
+
+   if (startPage > 2) {
+     pageNumbers.push(<span key="ellipsis-start">...</span>);
+   }
+
+   for (let page = startPage; page <= endPage; page++) {
+     pageNumbers.push(
+       <button
+         key={page}
+         onClick={() => handlePageChange(page)}
+         disabled={currentPage === page}
+       >
+         {page}
+       </button>
+     );
+   }
+
+   if (endPage < totalPages - 1) {
+     pageNumbers.push(<span key="ellipsis-end">...</span>);
+   }
+
+   // Добавляем последнюю страницу
+   if (totalPages > 1) {
+     pageNumbers.push(
+       <button
+         key={totalPages}
+         onClick={() => handlePageChange(totalPages)}
+         disabled={currentPage === totalPages}
+       >
+         {totalPages}
+       </button>
+     );
+   }
+
+   return pageNumbers;
+ };
 
   const filteredMovies = movies.filter((movie) => {
     if (selectedGenre && selectedLanguage) {
@@ -154,6 +226,22 @@ const Movies = () => {
           </li>
         ))}
       </ul>
+
+      <div>
+        <button
+          onClick={handlePageChange.bind(null, currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Назад
+        </button>
+        {renderPageNumbers()}
+        <button
+          onClick={handlePageChange.bind(null, currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Вперед
+        </button>
+      </div>
     </div>
   );
 };
