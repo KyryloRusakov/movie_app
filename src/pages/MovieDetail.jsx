ad542873ae7272b29e2858c3fcec8d62da0d4af3
Kyrylo Rusakov <kyrylo.rusakov@nixsolutions.com>
2023-06-09 12:36:05 +0300

    add movie to favorite with localstorage and remove it

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -4,7 +4,8 @@ import { useParams, useNavigate } from "react-router-dom";
 const MovieDetail = () => {
   const [movieDetail, setMovieDetail] = useState([]);
   const [movieGenres, setMovieGenres] = useState([]);
-  const [active, setActive] = useState(false);
+  // const [active, setActive] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(false);
 
   const { id } = useParams();
   const navigate = useNavigate();
@@ -26,16 +27,42 @@ const MovieDetail = () => {
     };
 
     fetchMovie();
-  }, []);
+  }, [id]);
 
   // const [movie, setMovie] = useState(null);
 
+  useEffect(() => {
+    // Проверка наличия фильма в списке выбранных при загрузке страницы
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    const isMovieInFavorites = favorites.some(
+      (favMovie) =>       {
+        console.log(favMovie.id) 
+        return favMovie.id === +id}
+      );
+      setIsFavorite(isMovieInFavorites);
+      console.log(isMovieInFavorites);
+      console.log(favorites);
+      console.log(id);
+
+  }, [id]);
+
   const addToFavorites = () => {
     const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-    favorites.push(movieDetail);
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-    // navigate("/favorites");
-    setActive(!active);
+    console.log(favorites);
+
+    if (isFavorite) {
+      // Удаление фильма из списка выбранных
+      const updatedFavorites = favorites.filter(
+        (favMovie) => favMovie.id !== movieDetail.id
+      );
+      setIsFavorite(false);
+      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    } else {
+      // Добавление фильма в список выбранных
+      favorites.push(movieDetail);
+      setIsFavorite(true);
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+    }
   };
 
   // if (!movie) {
@@ -70,7 +97,7 @@ const MovieDetail = () => {
               {Number(movieDetail.vote_average).toFixed(1)}
             </span>
           </div>
-          <button
+          {/* <button
             className={active ? "btn-favorite active" : "btn-favorite"}
             onClick={addToFavorites}
           >
@@ -83,12 +110,15 @@ const MovieDetail = () => {
             >
               {" "}
               <path
-              fill="white"
+                fill="white"
                 fill-rule="evenodd"
                 d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
                 className="icon-favorite"
               />
             </svg>
+          </button> */}
+          <button onClick={addToFavorites}>
+            {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
           </button>
           <div className="movie-detail-overview">{movieDetail.overview}</div>
         </div>
